fe553e3a975446b96a8d25a95ed2c9851299851c
Elena Silvana <mistysilvana@pop-os.localdomain>
2019-01-30 12:59:19 -0600

    función encode funcionando, funcion decode parcialmente funcionando, habilitado boton decifrar

--- src/index.js
+++ src/index.js
@@ -24,20 +24,66 @@ function encode (offset, str){
 	document.getElementById("outputText").innerHTML = arr;
 }
 
-//como llenar textarea con arr
-//innerHTML
+//medio sirve
+//LO9 GA:I:O9 9ON BONI:O9 = LOS GATITOS SON BONITOS
+//T, S
+function decode (offset, str){
 
-console.log(arr);
+	let arr = [];
+	str = str.toUpperCase();
+	for(let i = 0; i < str.length ; i++ ){
+
+		console.log('charCode ' + str.charCodeAt(i));
+		console.log(String.fromCharCode(str.charCodeAt(i)));
+		//para que el output text respete los espacios dentro del string
+		if (str.charCodeAt(i) === 32) {
+			console.log(String.fromCharCode(32));
+			arr.push(String.fromCharCode(32));
+		}
+		else {
+			console.log(String.fromCharCode(((str.charCodeAt(i) - 65 - offset) % 26 + 65)));
+			arr.push(String.fromCharCode(((str.charCodeAt(i) - 65 - offset) % 26 + 65)));
+			
+		}
+
+	}
+ arr = arr.join('');
+ //return arr
+ document.getElementById("outputText").innerHTML = arr;
+}
+
+
+
+//console.log(arr);
+
+//si offset está vacío mostrar mensaje de que hace falta llenar ese campo
 
 function cipher(){
 	//la variable que captura el desplazamiento deseado
 	let offset = parseInt(document.getElementById("offset").value);
 	//String es la variable que obtiene el texto ingresado por el usuario
 	let str = document.getElementById("inputText").value;
-	console.log('offset es ' + typeof offset);
+	console.log('offset es ' + offset);
 	console.log('String es '+ str);
 	encode(offset,str);
+	document.getElementById("resetElements").reset();
 };
+
+
+function decipher(){
+	//variable que captura el desplazamiento para descifrar
+	let offset = parseInt(document.getElementById("offset").value);
+	//String es la variable que obtiene el texto ingresado por el usuario
+	let str = document.getElementById("inputText").value;
+	console.log('offset es '+ offset);
+	console.log('String es '+ str);
+	decode(offset,str);
+	document.getElementById("resetElements").reset();
+
+
+}
+
+
 
 function reload(){
 	location.reload();
